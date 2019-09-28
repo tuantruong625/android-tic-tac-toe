a7780218084f329266d86ab06b1646b2be9c852b
Tuan Truong <tuan.truong625@gmail.com>
2019-09-28 13:26:16 -0400

    Change layout of grid

--- app/src/main/java/com/example/ttassignment01/MainActivity.java
+++ app/src/main/java/com/example/ttassignment01/MainActivity.java
@@ -1,27 +1,42 @@
 package com.example.ttassignment01;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener {
     Button aButtons[][] = new Button[3][3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        aButtons[0][0] = findViewById(R.id.button00);
+        aButtons[0][1] = findViewById(R.id.button01);
+        aButtons[0][2] = findViewById(R.id.button02);
+        aButtons[1][0] = findViewById(R.id.button10);
+        aButtons[1][1] = findViewById(R.id.button11);
+        aButtons[1][2] = findViewById(R.id.button12);
+        aButtons[2][0] = findViewById(R.id.button20);
+        aButtons[2][1] = findViewById(R.id.button21);
+        aButtons[2][2] = findViewById(R.id.button22);
 
-        aButtons[0][0] = findViewById(R.id.btnCell00);
-        aButtons[0][1] = findViewById(R.id.btnCell01);
-        aButtons[0][2] = findViewById(R.id.btnCell02);
-        aButtons[1][0] = findViewById(R.id.btnCell10);
-        aButtons[1][1] = findViewById(R.id.btnCell11);
-        aButtons[1][2] = findViewById(R.id.btnCell12);
-        aButtons[2][0] = findViewById(R.id.btnCell20);
-        aButtons[2][1] = findViewById(R.id.btnCell21);
-        aButtons[2][2] = findViewById(R.id.btnCell23);
+        // link delegate to the layout
+
+        for(int j = 0; j < 3; j++){
+            for(int i =  0; i < 3; i++){
+                aButtons[i][j].setOnClickListener(this);
+            }
+        }
+
+    }
+
+    @Override
+
+    public void onClick(View v){
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show();
     }
 }
